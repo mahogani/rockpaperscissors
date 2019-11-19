47483274d5d8e54bd5325d9c2a85446ed7501979
mahogani <mahoganimyles@gmail.com>
2019-11-19 23:17:40 +0000

    your message here

--- js/script.js
+++ js/script.js
@@ -4,14 +4,21 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
-
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function() {
-$("#shoot").click(function(){
-    $("#userChoice").text($("input").val());
-})
+    $("#shoot").click(function(){
+    
+        $("#userChoice").text($("input").val());
 
+        let computer=Math.random();
 
+        console.log(computer);
+        if (computer > 0 && computer < .33)  {
+            $("#computerChoice").text("rock");
+        } else if (computer  > .33  && computer < .66) {
+            $("#computerChoice").text("paper");
+        } else if ( computer > .66 && computer < .99) {
+            $("#computerChoice").text("scissors");
+        }
+    })
 });
