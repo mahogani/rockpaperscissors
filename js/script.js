7e80603a8fd3e787d5227ecb92459f655f166e93
mahogani <mahoganimyles@gmail.com>
2019-12-03 20:40:54 +0000

    comment goes here

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-// DOCUMENT READY FUNCTION BELOW
 $(document).ready(function() {
     $("#shoot").click(function(){
     
@@ -12,13 +5,40 @@ $(document).ready(function() {
 
         let computer=Math.random();
 
-        console.log(computer);
         if (computer > 0 && computer < .33)  {
             $("#computerChoice").text("rock");
-        } else if (computer  > .33  && computer < .66) {
+            
+        } else if (computer  > .34  && computer < .66) {
             $("#computerChoice").text("paper");
-        } else if ( computer > .66 && computer < .99) {
+            
+        } else if ( computer > .67 && computer < .99) {
             $("#computerChoice").text("scissors");
+        
+        } 
+        
+        
+        if ($("#userChoice").text() === "rock" && $("#computerChoice").text() === "scissors"){
+                alert ("You win!");
+                
+        } else if ($("#userChoice").text() === "paper" && $("#computerChoice").text() === "rock"){
+                alert ("You win!");
+                
+        } else if ($("#userChoice").text() === "scissors" && $("#computerChoice").text() === "paper"){
+                alert ("You win!");
+                
+        } else if ($("#computerChoice").text() === "rock" && $("#userChoice").text()  === "scissors"){
+                alert ("Sorry, you lose.");
+                
+        } else if ($("#computerChoice").text() === "paper" && $("#userChoice").text()  === "rock"){
+                alert ("Sorry, you lose.");
+                
+        } else if ($("#computerChoice").text() === "scissors" && $("#userChoice").text() === "paper"){
+                alert ("Sorry, you lose.");
+                
+        } else  if ( $("userChoice").attr("id") ===  $("#computerChoice").text()){
+                alert ("Tie!");
         }
-    })
-});
+    });    
+            
+
+    });
